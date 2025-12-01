644a9bdf76f1850e6be9bb50557618b56ef9aa06
Fatih Yalcin <yalcin_fatih@web.de>
2025-12-01 11:10:09 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,146 @@
+let contentRef;
+let basketRef;
+let menuRef;
+let basketDesktopRef;
+let basketMobileRef;
+let basketDialog;
+
+
+
+
+
+let myDishs = [
+    {"name" : "Etli Ekmek",
+        "price" : 9.90,
+        "description" : "Dünner, länglicher Teigfladen, belegt mit fein gehacktem Rindfleisch, Gewürzen und Gemüse. Zubereitet im traditionellem Steinofen für den ganz besonderen knusprigen und rustikalen Geschmack ",
+    },
+
+    {
+        "name" : "Mevlana",
+        "price" : 10.90,
+        "description" : "Dünner, länglicher Teigfladen belegt mit gehacktem Rindfleisch und Käse. Zubereitet im traditionellem Steinofen für den ganz besonderen knusprigen und rustikalen Geschmack"
+
+    }, 
+
+    {
+        "name" : "Bicak Arasi",
+        "price" : 11.90,
+        "description" : "Dünner, länglicher Teigfladen belegt mir sehr fein geschnittenem Rindfleisch, Gewürzen und Gemüse. Zubereitet im tradittionllem Steinofen für den ganz besonderen knusprigen und rustikalen Geschmack",
+
+    },
+
+    {
+        "name" : "Tirrit",
+        "price" : 13.90,
+        "description" : "Ein Gericht bei dem klein geschnittenes Fladenbrot mit zartem Fleisch vereint, welches mit kräftiger Fleischbrühe und Tomatensoße übergossen wird",
+    },
+
+      {
+        "name" : "Firin Kebabi",
+        "price" : 19.90,
+        "description" : "Zart geschmortes Lammfleisch aus dem traditionellen Steinofen welches mit frischen gemüse und selbst gemachten Fladenbrot serviert wird",
+    },
+
+
+];
+
+let cart = [];
+
+
+function init () {
+
+contentRef = document.getElementById ('content');
+basketRef = document.getElementById ('basket');
+menuRef = document.getElementById ('menu');
+basketDesktopRef = document.getElementById ('basket'); 
+basketMobileRef = document.getElementById ('basket-mobile');
+basketDialog = document.getElementById ('basketDialog');
+
+loadCart();
+renderDishs();
+};
+
+function renderDishs () {
+    contentRef.innerHTML = "";
+
+    for (let i =0; i < myDishs.length; i++) {
+        let dish = myDishs[i];
+
+        contentRef.innerHTML += getDishTemplate(i);
+    }
+}
+
+function addToCart(i) {
+    let cartEntry = cart.find(entry => entry.index === i);
+
+    if (cartEntry) {
+        cartEntry.amount++;
+    }
+
+    else {
+        cart.push({
+            index:i,
+            amount:1
+        });
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
+}
+
+function renderCart() {
+  basketRef.innerHTML = `
+    <h2 class='basket-header'>Warenkorb</h2>
+    <div id="basket-items"></div>
+    <div class="basket-total-row">
+      <span>Gesamt:</span>
+      <span id="basket-total"></span>
+    </div>
+  `;
+
+  
+  let itemsRef = basketRef.querySelector('#basket-items');
+  let totalRef = basketRef.querySelector('#basket-total');
+
+  if (cart.length === 0) {
+    itemsRef.innerHTML = "<p>Der Warenkorb ist leer</p>";
+    totalRef.textContent = "0,00 €";
+    return;
+  }
+
+  let total = 0;
+
+  for (let i = 0; i < cart.length; i++) {
+    let cartEntry = cart[i];
+    let dish = myDishs[cartEntry.index];
+    let itemTotal = dish.price * cartEntry.amount;
+    total += itemTotal;
+
+    itemsRef.innerHTML += getCartItemTemplate(i);
+  }
+
+  totalRef.textContent = total.toFixed(2) + " €";
+}
+
+function loadCart() {
+    let cartJSON = localStorage.getItem("cart");
+
+    if (cartJSON) {
+        cart = JSON.parse(cartJSON);
+    }
+
+    renderCart();
+}
+
+function openBasketDialog() {
+
+  basketRef = basketMobileRef;
+  renderCart();               
+  basketDialog.showModal();
+}
+
+function closeBasketDialog() {
+  basketDialog.close();
+  basketRef = basketDesktopRef;
+  renderCart();          
+}
---
+++ scripts/templates.js
@@ -0,0 +1,48 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+}
+
+function getDishTemplate(i) {
+    let dish = myDishs[i];
+
+    return`
+    <article class="dish-card">
+    <h3>${dish.name} - ${dish.price.toFixed(2)} €</h3>
+   <p>${dish.description} </p>
+   <button onclick ="addToCart(${i})">+</button>
+   </article> `;
+}
+
+function getCartItemTemplate(cartIndex) {
+    let entry = cart[cartIndex];
+    let dish = myDishs[entry.index];
+    let subTotal = dish.price * entry.amount;
+
+    return `
+    <article class="cart-item">
+        <div class="cart-item-main">
+            <span class="cart-item-name">${dish.name}</span>
+            <span class="cart-item-price">${subTotal.toFixed(2)} €</span>
+        </div>
+        <div class="cart-item-controls">
+            <button onclick="changeAmount(${cartIndex}, -1)">-</button>
+            <span>${entry.amount}</span>
+            <button onclick="changeAmount(${cartIndex}, 1)">+</button>
+        </div>
+    </article>
+    `;
+}
+
+
+function changeAmount(cartIndex, delta) {
+    cart[cartIndex].amount += delta;
+
+    if (cart[cartIndex].amount <= 0) {
+        
+        cart.splice(cartIndex, 1);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
+}
